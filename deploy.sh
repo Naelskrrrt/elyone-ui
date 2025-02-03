6b2b5997abdc5bc9a5507c0ce72e2ffdddc9b368
NaelSkrrrt <ralalanael@gmail.com>
2025-02-04 00:28:55 +0300

    Fix deployment issue

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ echo "Creating an optimized production build..."
 pnpm run build
 
 echo "Deploying the build to the server..."
-scp -r dist/* root@137.74.220.203:/var/www/html/
+scp -P 222 -r dist/* root@137.74.220.203:/var/www/html/
 
 echo "Send Modification to Github"
 git add .
-git commit -m "Deploy"
+git commit -m "Fix deployment issue"
 git push -u origin dev
