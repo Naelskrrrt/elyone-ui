942e2755eff28a8074d5e6ae9f64166b479aede0
NaelSkrrrt <ralalanael@gmail.com>
2025-02-05 01:24:01 +0300

    IMPROVE: env for https

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -6,7 +6,7 @@ export const fetchClient = async (params: {
 }): Promise<{ client_name: string }> => {
     const { id_erp, hubspot_id } = params;
     const response = await axios.get<{ client_name: string }>(
-        "http://137.74.220.203:8081/api/nomClient",
+        import.meta.env.VITE_API_URL + "/api/nomClient",
         {
             params: {
                 id_erp,
